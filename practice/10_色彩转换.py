c5db12a68bf61811e09c8a6393f0734d76def23b
Connor <xiaobang199806@163.com>
2023-04-14 17:58:22 +0800

    update

--- practice/10_色彩转换.py
+++ practice/10_色彩转换.py
@@ -1,4 +1,27 @@
-# 将图5.1从BGR色彩空间转换到GRAY色彩空间
+import cv2
+import matplotlib.pyplot as plt
 
+# 将图5.1从BGR色彩空间转换到GRAY色彩空间
+img = cv2.imread('../imgs/5.1.jpg')
+cv2.imshow('img', img)
+# 转换色彩空间
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('gray', img_gray)
+cv2.waitKey()
+cv2.destroyAllWindows()
 
 # 将图5.1从BGR色彩空间转换到HSV色彩空间
+img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+cv2.imshow('hsv', img_hsv)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+# 使用matplotlib进行显示，显示的默认通道是RGB
+# 如果不将图片由BGR转换为RGB，显示的图像颜色会出错
+plt.imshow(img)
+plt.show()
+# 通过切片转换
+img_RGB = img[:, :, ::-1]
+# 还可通过cvt方法转换: img_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.imshow(img_RGB)
+plt.show()
