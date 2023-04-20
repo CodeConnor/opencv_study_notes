c653a5b72d5299aba6f03f50ce77ef146540974e
Connor <xiaobang199806@163.com>
2023-04-20 19:09:27 +0800

    update eg.4

--- practice/19_warpAffine仿射变换.py
+++ practice/19_warpAffine仿射变换.py
@@ -27,3 +27,38 @@ cv2.destroyAllWindows()
 # ----------------------------------------------------eg.3
 # 让图像向右倾斜（图像左上角的点向右移动50像素）
 # 使用eg.1中的图像信息：img、rows、cols
+p1 = np.zeros((3, 2), np.float32)  # 32位浮点型空列表，原图3个点
+p1[0] = [0, 0]          # 左上角A点坐标
+p1[1] = [cols-1, 0]     # 右上角B点坐标
+p1[2] = [0, rows-1]     # 左下角C点坐标
+p2 = np.zeros((3, 2), np.float32)  # 32位浮点型空列表，倾斜图3个点
+p2[0] = [50, 0]         # 左上角A点坐标，向右移动50像素
+p2[1] = [cols-1, 0]     # 右上角B点坐标，位置不变
+p2[2] = [0, rows-1]     # 左下角C点坐标，位置不变
+M = cv2.getAffineTransform(p1, p2)  # 计算M矩阵
+dst3 = cv2.warpAffine(img, M, (cols, rows))
+cv2.imshow('img', img)
+cv2.imshow('dst3', dst3)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+# ----------------------------------------------------eg.4
+# 模拟从底部观察图像得到的透视效果，将图像顶部边缘收窄90像素，底部边缘保持不变
+# 使用eg.1中的图像信息：img、rows、cols
+p1 = np.zeros((4, 2), np.float32)  # 32位浮点型空列表，原图4个点
+p1[0] = [0, 0]              # 左上角A点坐标
+p1[1] = [cols-1, 0]         # 右上角B点坐标
+p1[2] = [0, rows-1]         # 左下角C点坐标
+p1[3] = [cols-1, rows-1]    # 右下角D点坐标
+p2 = np.zeros((4, 2), np.float32)  # 32位浮点型空列表，倾斜图4个点
+p2[0] = [90, 0]             # 左上角A点坐标，向右移动90像素
+p2[1] = [cols-1-90, 0]      # 右上角A点坐标，向左移动90像素
+p2[2] = [0, rows-1]         # 左下角C点坐标，位置不变
+p2[3] = [cols-1, rows-1]    # 右下角C点坐标，位置不变
+M = cv2.getPerspectiveTransform(p1, p2)  # 计算变换矩阵
+dst4 = cv2.warpPerspective(img, M, (cols, rows))  # 三维空间的仿射
+cv2.imshow('img', img)
+cv2.imshow('dst4', dst4)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
